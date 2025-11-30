c37e90802da68ad826825aeb2fd682d963ad84a8
Gaurav Singh <108992402+gv-sh@users.noreply.github.com>
2025-12-01 09:51:07 +1000

    remove redundant copies

--- user/src/lib/utils.js
+++
@@ -1,9 +0,0 @@
-import { clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
- 
-/**
- * Merge Tailwind CSS classes conditionally
- */
-export function cn(...inputs) {
-  return twMerge(clsx(inputs))
-}
--- user/src/utils/deviceUtils.js
+++
@@ -1,4 +0,0 @@
-export const isMobileDevice = () => {
-  const userAgent = navigator.userAgent.toLowerCase();
-  return /android|webos|iphone|ipod|blackberry|iemobile|opera mini/i.test(userAgent);
-};
